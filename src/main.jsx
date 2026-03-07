45a4100b62a9ea46843c4b29281e88bacd837618
abene <abenezertileye2003@gmail.com>
2026-03-07 12:38:32 +0300

    integration with firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
+import AuthProvider from "./AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 <React.StrictMode>
 <BrowserRouter>
-<App />
+<AuthProvider>
+  <App />
+</AuthProvider>
 </BrowserRouter>
 </React.StrictMode>
 );
