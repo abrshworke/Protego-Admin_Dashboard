afb6e5e07b0564f09d18d2a7e18fffdc8405fd88
abene <abenezertileye2003@gmail.com>
2026-03-07 12:38:32 +0300

    integration with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import AuthorityLogin from "./page/AuthorityLogin";
+
 import Overview from "./page/Overview";
 import Incidents from "./page/Incidents";
 import AnonymousReports from "./page/AnonymousReports";
 import UserManagement from "./page/UserManagement";
 import LiveMap from "./page/LiveMap";
-
+import AdminDashboard  from "./page/AdminDashboard";
 
 export default function App() {
-return (
-<Routes> 
-    
+  return (
+    <Routes>
+      {/* Public */}
+      <Route path="/login" element={<AuthorityLogin />} />
+
+      {/* Protected Routes */}
+      <Route element={<ProtectedRoute />}>
         <Route path="/" element={<Overview />} />
         <Route path="/incidents" element={<Incidents />} />
         <Route path="/reports" element={<AnonymousReports />} />
         <Route path="/users" element={<UserManagement />} />
-        <Route path="/map" element={<LiveMap />} /> 
+        <Route path="/map" element={<LiveMap />} />
+      </Route>
 
-</Routes>
-);
+      <Route element={<ProtectedRoute adminOnly={true} />}>
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Route>
+    </Routes>
+  );
 }
-
