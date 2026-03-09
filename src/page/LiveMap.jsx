12570d0f21336ffa296ff3e16e276ee736ec3d52
UGR/2898/13 <147318783+abrshworke@users.noreply.github.com>
2026-03-09 20:57:18 +0300

    Update LiveMap.jsx

--- src/page/LiveMap.jsx
+++ src/page/LiveMap.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef, useContext } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import SideBar from "../components/SideBar";
+import Sidebar from "../components/SideBar";
 import Topbar from "../components/Topbar";
 import L from "leaflet";
 import Footer from "../components/Footer";
