fe1789f236982003e624794d12d9c2f2473a9138
UGR/2898/13 <147318783+abrshworke@users.noreply.github.com>
2026-03-09 20:57:36 +0300

    Rename import from SideBar to Sidebar

--- src/page/Overview.jsx
+++ src/page/Overview.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import SideBar from "../components/SideBar";
+import Sidebar from "../components/SideBar";
 import Topbar from "../components/Topbar";
 import StatCard from "../components/StatCard";
 import Footer from "../components/Footer";
